5d0c177555cca6f3e554d2e6c10fdf55cd853a8b
thegibi <gibifyofficial@gmail.com>
2025-10-14 21:10:13 -0400

    fix: remove accelerate

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,9 @@
-import { withAccelerate } from "@prisma/extension-accelerate";
 import { PrismaClient } from "~/lib/generated/prisma-client";
 
-const prisma = new PrismaClient().$extends(withAccelerate());
+const prisma = new PrismaClient();
 
 const globalForPrisma = global as unknown as { prisma: typeof prisma };
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-export default prisma;
+export default globalForPrisma.prisma || prisma;
